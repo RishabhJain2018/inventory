604fa0a4c6cc2d06ffefd2fd340d0a3d3fa1540a
RishabhJain2018 <rishabhjain2018@gmail.com>
2016-11-06 20:21:10 +0530

    Part 11 complete

--- products/serializers.py
+++ products/serializers.py
@@ -7,35 +7,35 @@ from .models import Category, Stat, Product
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = User
-		exclude = ('password',)
+    class Meta:
+        model = User
+        exclude = ('password',)
 
 
 class CategorySerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Category
+    class Meta:
+        model = Category
 
 
 class StatSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Stat
+    class Meta:
+        model = Stat
 
 
 class ProductSerializer(serializers.ModelSerializer):
-	# category_id = serializers.PrimaryKeyRelatedField(
-	# 	write_only=True,
-	# 	queryset=Category.objects.all(),
-	# 	source='category',
-	# 	)
-	category = CategorySerializer()
+    category_id = serializers.PrimaryKeyRelatedField(
+        write_only=True,
+        queryset=Category.objects.all(),
+        source='category',
+        )
+    category = CategorySerializer(read_only=True)
 
-	class Meta:
-		model = Product
-		# fields = ('id', 'name', 'category',)
+    class Meta:
+        model = Product
+        fields = ('id', 'name', 'category_id', 'category',)
 
-	def create(self, validated_data):
-		category_data = validated_data.pop('category')
-		category = Category.objects.get(**category_data)
-		product = Product.objects.create(category=category, **validated_data)
-		return product
+    # def create(self, validated_data):
+    #   category_data = validated_data.pop('category')
+    #   category = Category.objects.get(**category_data)
+    #   product = Product.objects.create(category=category, **validated_data)
+    #   return product
